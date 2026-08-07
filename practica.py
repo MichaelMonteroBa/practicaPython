d88900db25601a1fd2b613047de650a650028b6e
Michael Montero <monterobautistamichael@gmail.com>
2026-08-07 19:40:35 -0400

    renombro archivo de practica

--- practica.py
+++
@@ -1,7 +0,0 @@
-# calcula el area de un rectangulo
-print("ingrese la base:")
-base = float(input())
-print ("ingrese la altura:")
-altura = float (input())
-area = base * altura
-print(f"El area es de: {area}")
